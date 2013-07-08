60133b1380f0335feffd8a698c324db6286efd75
Casey DeLorme <CDeLorme@gmail.com>
2013-07-07 20:47:40 -0400

    Added gui config, needs refining for modularity.

--- linux/debian/wheezy/template/setup.sh
+++ linux/debian/wheezy/template/setup.sh
@@ -78,97 +78,161 @@ setup_automatic_updates()
     chmod +x /etc/cron.weekly/aptitude
 }
 
-
-
-
-
-
-
-
-setup_firewall()
+load_git_config()
 {
+    if [ -n "$CONFIGURE_GIT" ] && $CONFIGURE_GIT;then
+        echo "Adding Git Helpers."
 
-    echo "Setting up firewall."
+        # Move awesome helper files
+        cp -ra $FILES/git/. /etc/skel
+        cp -ra $FILES/git/. /root
 
-    # Xen generates vifs dynamically
-    # Securing that without a script would be very difficult
-    # So we use a blacklist instead of a whitelist to control what we know
-
-    # Define firewall at `/etc/firewall.conf`
-    if $DUAL_LAN;then
-        echo "*filter\n\n# Prevent use of Loopback on non-loopback dervice (lo0):\n-A INPUT -i lo -j ACCEPT\n-A INPUT ! -i lo -d 127.0.0.0/8 -j REJECT\n\n# Accepts all established inbound connections\n-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT\n\n# Allows all outbound traffic (Can be limited at discretion)\n-A OUTPUT -j ACCEPT\n\n# Allow ping\n-A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT\n\n# Enable SSH Connection (custom port in /etc/ssh/sshd_conf)\n-A INPUT -p tcp -m state --state NEW --dport $SSH_PORT -j ACCEPT\n\n# Forwarding Rules (for Dual LAN Xen)\n-A FORWARD -i eth0 -o eth1 -j REJECT\n-A FORWARD -i eth0 -o xenbr1 -j REJECT\n-A FORWARD -i eth1 -o eth0 -j REJECT\n-A FORWARD -i eth1 -o xenbr0 -j REJECT\n\n# Set other traffic defaults\n-A INPUT -j REJECT\n-A FORWARD -j ACCEPT\n\nCOMMIT" > /etc/firewall.conf
-    else
-        echo "*filter\n\n# Prevent use of Loopback on non-loopback dervice (lo0):\n-A INPUT -i lo -j ACCEPT\n-A INPUT ! -i lo -d 127.0.0.0/8 -j REJECT\n\n# Accepts all established inbound connections\n-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT\n\n# Allows all outbound traffic (Can be limited at discretion)\n-A OUTPUT -j ACCEPT\n\n# Allow ping\n-A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT\n\n# Enable SSH Connection (custom port in /etc/ssh/sshd_conf)\n-A INPUT -p tcp -m state --state NEW --dport $SSH_PORT -j ACCEPT\n\n# Forwarding Rules (for Dual LAN Xen)\n-A FORWARD -i eth0 -o xenbr1 -j REJECT\n\n# Set other traffic defaults\n-A INPUT -j REJECT\n-A FORWARD -j ACCEPT\n\nCOMMIT" > /etc/firewall.conf
+        # Load any awesome saved git configs
+        echo "\n\t[alias]\n\t\tl = \"!. ~/.githelpers && pretty_git_log\"" >> /root/.gitconfig
+        echo "\n# Git Additions\n. ~/.git-completion\n. ~/.git-prompt" >> /root/.profile
+        echo "\n\t[alias]\n\t\tl = \"!. ~/.githelpers && pretty_git_log\"" >> /etc/skel/.gitconfig
+        echo "\n# Git Additions\n. ~/.git-completion\n. ~/.git-prompt" >> /etc/skel/.profile
     fi
-
-    # Prepare firewall auto-loading
-    echo "#!/bin/sh\niptables -F\niptables-restore < /etc/firewall.conf" > "/etc/network/if-up.d/iptables"
-    chmod +x "/etc/network/if-up.d/iptables"
-
-}
-
-git_config()
-{
-
-    echo "Adding Git Helpers."
-
-    # Move awesome helper files
-    cp -ra $FILES/git/. /etc/skel
-    cp -ra $FILES/git/. /root
-
-    # Load any awesome saved git configs
-    echo "\n\t[alias]\n\t\tl = \"!. ~/.githelpers && pretty_git_log\"" >> /root/.gitconfig
-    echo "\n# Git Additions\n. ~/.git-completion\n. ~/.git-prompt" >> /root/.profile
-    echo "\n\t[alias]\n\t\tl = \"!. ~/.githelpers && pretty_git_log\"" >> /etc/skel/.gitconfig
-    echo "\n# Git Additions\n. ~/.git-completion\n. ~/.git-prompt" >> /etc/skel/.profile
-
 }
 
 git_user_config()
 {
+    if [ -n "$CONFIGURE_GIT" ] && $CONFIGURE_GIT;then
+        echo "Configuring Git."
 
-    echo "Configuring Git."
+        # Run global config as root
+        if [ ! -z "$GIT_NAME" ];then
+            git config --global user.name "$GIT_NAME"
+        fi
+        if [ ! -z "$GIT_EMAIL" ];then
+            git config --global user.email "$GIT_EMAIL"
+        fi
+        git config --global core.editor "vim"
+        git config --global help.autocorrect -1
+        git config --global color.ui true
 
-    # Run global config as root
-    if [ ! -z "$GIT_NAME" ];then
-        git config --global user.name "$GIT_NAME"
+        # Run global config as user
+        su -c "git config --global user.name \"$GIT_NAME\"" $USERNAME
+        su -c "git config --global user.email \"$GIT_EMAIL\"" $USERNAME
+        su -c "git config --global core.editor \"vim\"" $USERNAME
+        su -c "git config --global help.autocorrect -1" $USERNAME
+        su -c "git config --global color.ui true" $USERNAME
     fi
-    if [ ! -z "$GIT_EMAIL" ];then
-        git config --global user.email "$GIT_EMAIL"
-    fi
-    git config --global core.editor "vim"
-    git config --global help.autocorrect -1
-    git config --global color.ui true
-
-    # Run global config as user
-    su -c "git config --global user.name \"$GIT_NAME\"" $USERNAME
-    su -c "git config --global user.email \"$GIT_EMAIL\"" $USERNAME
-    su -c "git config --global core.editor \"vim\"" $USERNAME
-    su -c "git config --global help.autocorrect -1" $USERNAME
-    su -c "git config --global color.ui true" $USERNAME
-
 }
 
 user_configuration()
 {
+    if [ -n "$USERNAME" ]; then
+        echo "Configure Supplied User: $USERNAME."
 
-    echo "Configure Supplied User: $USERNAME."
+        # Create user if not exists
+        if ! id -u "$USERNAME" >/dev/null 2>&1;then
+            if [ -n "$PASSWORD" ];then
+                useradd -m -s /bin/bash -p $(mkpasswd -m md5 $PASSWORD) $USERNAME
+            else
+                echo "You will have to add a password to the $USERNAME from root with passwd..."
+                useradd -m -s /bin/bash $USERNAME
+            fi
+        fi
 
-    # Create user if not exists
-    if ! id -u "$USERNAME" >/dev/null 2>&1 && [ ! -z "$PASSWORD" ]; then
-        useradd -m -s /bin/bash -p $(mkpasswd -m md5 $PASSWORD) $USERNAME
+        # Add to sudo group
+        usermod -aG sudo $USERNAME
+
+        # Create important user directories
+        mkdir -p /home/$USERNAME/.config/autostart
+
+        # Update Ownership
+        chown -R $USERNAME:$USERNAME /home/$USERNAME
     fi
-
-    # Add to sudo group
-    usermod -aG sudo $USERNAME
-
-    # Create important user directories
-    mkdir -p /home/$USERNAME/.config/autostart
-
-    # Update Ownership
-    chown -R $USERNAME:$USERNAME /home/$USERNAME
-
 }
+
+
+
+
+
+
+
+
+# gui_configuration()
+# {
+#     echo "Modifying Runlevel Kernel Components."
+
+#     # Adjustments for gui settings
+#     update-rc.d gdm3 disable 2
+#     update-rc.d network-manager disable 2
+#     update-rc.d network-manager disable 3
+#     update-rc.d network-manager disable 4
+#     update-rc.d network-manager disable 5
+#     update-rc.d bluetooth disable 2
+#     update-rc.d bluetooth disable 3
+#     update-rc.d bluetooth disable 4
+#     update-rc.d bluetooth disable 5
+
+#     echo "Patching Guake & setting to Autostart."
+
+#     # Patch Guake Gnome3 notification bug and remove autostart prevention
+#     sed -i 's/notification.show()/try:\n                notification.show()\n            except Exception:\n                pass/' /usr/bin/guake
+#     rm /etc/xdg/autostart/guake.desktop
+#     sed -i '/StartupNotify|X-GNOME-Autostart-enabled/d' /usr/share/applications/guake.desktop
+
+#     # Add autostart for guake to user or global if no user
+#     if [ ! -z "$USERNAME" ];then
+#         ln -s /usr/share/applications/guake.desktop /home/$USERNAME/.config/autostart/guake.desktop
+#         chown -R $USERNAME:$USERNAME /home/$USERNAME/.config
+#     else
+#         ln -s /usr/share/applications/guake.desktop /etc/xdg/autostart/guake.desktop
+#     fi
+
+#     echo "Setting up Sublime Text 2."
+
+#     # Sublime Text 2
+#     wget -O $PWD/sublime.tar.bz2 "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2"
+#     tar xf sublime.tar.bz2
+#     rm $PWD/*.bz2
+#     mv Sublime* /usr/share/sublime_text
+#     ln -s /usr/share/sublime_text/sublime_text /usr/bin/subl
+#     echo "[Desktop Entry]\nName=Sublime Text 2\nComment=The Best Text Editor in the World!\nTryExec=subl\nExec=subl\nIcon=/usr/share/sublime_text/Icon/256x256/sublime_text.png\nType=Application\nCategories=Office;Sublime Text;" > /usr/share/applications/subl.desktop
+#     echo "text/plain=subl.desktop\ntext/css=subl.desktop\ntext/htm=subl.desktop\ntext/javascript=subl.desktop\ntext/x-c=subl.desktop\ntext/csv=subl.desktop\ntext/x-java-source=subl.desktop\ntext/java=subl.desktop\n" >> /usr/share/applications/defaults.list
+#     update-desktop-database
+
+#     # Add User Configuration
+#     if [ -d $FILES/sublime_text ] && [ ! -z "$USERNAME" ];then
+#         mkdir -p /home/$USERNAME/.config/sublime-text-2
+#         cp -R $FILES/sublime_text/* /home/$USERNAME/.config/sublime-text-2/
+#         chown -R $USERNAME:$USERNAME /home/$USERNAME/.config/
+#     fi
+
+# }
+
+
+
+
+# Disabled until I can cleanup
+
+# setup_firewall()
+# {
+#     if [ -n "$SETUP_FIREWALL" ] && $SETUP_FIREWALL;then
+#         echo "Setting up firewall."
+
+#         # Xen generates vifs dynamically
+#         # Securing that without a script would be very difficult
+#         # So we use a blacklist instead of a whitelist to control what we know
+
+#         # Define firewall at `/etc/firewall.conf`
+#         if $DUAL_LAN;then
+#             echo "*filter\n\n# Prevent use of Loopback on non-loopback dervice (lo0):\n-A INPUT -i lo -j ACCEPT\n-A INPUT ! -i lo -d 127.0.0.0/8 -j REJECT\n\n# Accepts all established inbound connections\n-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT\n\n# Allows all outbound traffic (Can be limited at discretion)\n-A OUTPUT -j ACCEPT\n\n# Allow ping\n-A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT\n\n# Enable SSH Connection (custom port in /etc/ssh/sshd_conf)\n-A INPUT -p tcp -m state --state NEW --dport $SSH_PORT -j ACCEPT\n\n# Forwarding Rules (for Dual LAN Xen)\n-A FORWARD -i eth0 -o eth1 -j REJECT\n-A FORWARD -i eth0 -o xenbr1 -j REJECT\n-A FORWARD -i eth1 -o eth0 -j REJECT\n-A FORWARD -i eth1 -o xenbr0 -j REJECT\n\n# Set other traffic defaults\n-A INPUT -j REJECT\n-A FORWARD -j ACCEPT\n\nCOMMIT" > /etc/firewall.conf
+#         else
+#             echo "*filter\n\n# Prevent use of Loopback on non-loopback dervice (lo0):\n-A INPUT -i lo -j ACCEPT\n-A INPUT ! -i lo -d 127.0.0.0/8 -j REJECT\n\n# Accepts all established inbound connections\n-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT\n\n# Allows all outbound traffic (Can be limited at discretion)\n-A OUTPUT -j ACCEPT\n\n# Allow ping\n-A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT\n\n# Enable SSH Connection (custom port in /etc/ssh/sshd_conf)\n-A INPUT -p tcp -m state --state NEW --dport $SSH_PORT -j ACCEPT\n\n# Forwarding Rules (for Dual LAN Xen)\n-A FORWARD -i eth0 -o xenbr1 -j REJECT\n\n# Set other traffic defaults\n-A INPUT -j REJECT\n-A FORWARD -j ACCEPT\n\nCOMMIT" > /etc/firewall.conf
+#         fi
+
+#         # Prepare firewall auto-loading
+#         echo "#!/bin/sh\niptables -F\niptables-restore < /etc/firewall.conf" > "/etc/network/if-up.d/iptables"
+#         chmod +x "/etc/network/if-up.d/iptables"
+#     fi
+# }
+
+
+
 
 
 
