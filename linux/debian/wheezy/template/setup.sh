fbffd937fdebb92e29514146d34b01bcad415a0a
Casey DeLorme <CDeLorme@gmail.com>
2013-07-08 03:02:12 -0400

    Cleaned up a number of features.

--- linux/debian/wheezy/template/setup.sh
+++ linux/debian/wheezy/template/setup.sh
@@ -17,112 +17,6 @@ TEMPLATE_PATH=$(dirname $TEMPLATE_SCRIPT)
 
 # -------------------------------- Library Methods
 
-add_jis_locale()
-{
-    PACKAGES="$PACKAGES fonts-takao"
-    echo "ja_JP.UTF-8 UTF-8" >> /etc/locale.gen
-    locale-gen
-}
-
-ssd_trim_config()
-{
-    if [ -n "TRIM" ] && $TRIM; then
-        echo "Add trim to ssh & weekly crontab for file systems."
-
-        # Add discard flag to LVMs and execute it manually every week via crontab
-        sed -i 's/issue_discards = 0/issue_discards = 1/' /etc/lvm/lvm.conf
-        echo "#!/bin/sh\nfor mount in / /boot /home /var/log /tmp; do\n\tfstrim $mount\ndone" > /etc/cron.weekly/fstab
-        chmod +x /etc/cron.weekly/fstab
-    fi
-}
-
-install_fonts()
-{
-    if [ -n "$INSTALL_FONTS" ] && $INSTALL_FONTS;then
-        echo "Install custom fonts."
-
-        # Install my fonts
-        mkdir -p /usr/share/fonts/truetype
-        if [ -d $FILES/fonts/ ];then
-            mv $FILES/fonts/*.ttf /usr/share/fonts/truetype
-            fc-cache -rf
-        fi
-    fi
-}
-
-ssh_config()
-{
-    if [ -n "$SSH_PORT" ];then
-        echo "Update SSH Port."
-
-        # Set SSH Port
-        sed -i "s/Port 22/Port $SSH_PORT/" /etc/ssh/sshd_config
-        service ssh restart
-    fi
-}
-
-setup_automatic_updates()
-{
-    if [ -n "$AUTOMATIC_UPDATES" ] && $AUTOMATIC_UPDATES;then
-    echo "Creating Automatic Updates."
-
-    # Create a file to be executed weekly by crontabs:
-    echo "#!/bin/sh" >> /etc/cron.weekly/aptitude
-    echo "# Weekly Software Update Processing" >> /etc/cron.weekly/aptitude
-    echo "aptitude clean" >> /etc/cron.weekly/aptitude
-    echo "aptitude update" >> /etc/cron.weekly/aptitude
-    echo "aptitude upgrade -y || aptitude upgrade -y" >> /etc/cron.weekly/aptitude
-    echo "aptitude safe-upgrade -y || aptitude safe-upgrade -y" >> /etc/cron.weekly/aptitude
-
-    # Make it executable
-    chmod +x /etc/cron.weekly/aptitude
-}
-
-load_git_config()
-{
-    if [ -n "$CONFIGURE_GIT" ] && $CONFIGURE_GIT;then
-        echo "Adding Git Helpers."
-
-        # Move awesome helper files
-        cp -ra $FILES/git/. /etc/skel
-        cp -ra $FILES/git/. /root
-
-        # Load any awesome saved git configs
-        echo "\n\t[alias]\n\t\tl = \"!. ~/.githelpers && pretty_git_log\"" >> /root/.gitconfig
-        echo "\n# Git Additions\n. ~/.git-completion\n. ~/.git-prompt" >> /root/.profile
-        echo "\n\t[alias]\n\t\tl = \"!. ~/.githelpers && pretty_git_log\"" >> /etc/skel/.gitconfig
-        echo "\n# Git Additions\n. ~/.git-completion\n. ~/.git-prompt" >> /etc/skel/.profile
-    fi
-}
-
-git_user_config()
-{
-    if [ -n "$CONFIGURE_GIT" ] && $CONFIGURE_GIT;then
-        echo "Configuring Git."
-
-        # Run global config as root
-        if [ ! -z "$GIT_NAME" ];then
-            git config --global user.name "$GIT_NAME"
-        fi
-        if [ ! -z "$GIT_EMAIL" ];then
-            git config --global user.email "$GIT_EMAIL"
-        fi
-        git config --global core.editor "vim"
-        git config --global help.autocorrect -1
-        git config --global color.ui true
-
-        # Run global config as user
-        su -c "git config --global user.name \"$GIT_NAME\"" $USERNAME
-        su -c "git config --global user.email \"$GIT_EMAIL\"" $USERNAME
-        su -c "git config --global core.editor \"vim\"" $USERNAME
-        su -c "git config --global help.autocorrect -1" $USERNAME
-        su -c "git config --global color.ui true" $USERNAME
-    fi
-}
-
-
-
-
 
 kernel_cleanup()
 {
@@ -138,86 +32,6 @@ kernel_cleanup()
 #     update-rc.d bluetooth disable 4
 #     update-rc.d bluetooth disable 5
 }
-
-guake_config()
-{
-    echo "setting up guake..."
-#     # Patch Guake Gnome3 notification bug and remove autostart prevention
-#     sed -i 's/notification.show()/try:\n                notification.show()\n            except Exception:\n                pass/' /usr/bin/guake
-#     rm /etc/xdg/autostart/guake.desktop
-#     sed -i '/StartupNotify|X-GNOME-Autostart-enabled/d' /usr/share/applications/guake.desktop
-
-#     # Add autostart for guake to user or global if no user
-#     if [ ! -z "$USERNAME" ];then
-#         ln -s /usr/share/applications/guake.desktop /home/$USERNAME/.config/autostart/guake.desktop
-#         chown -R $USERNAME:$USERNAME /home/$USERNAME/.config
-#     else
-#         ln -s /usr/share/applications/guake.desktop /etc/xdg/autostart/guake.desktop
-#     fi
-# # Create important user directories
-# mkdir -p /home/$USERNAME/.config/autostart
-
-# # Update Ownership
-# chown -R $USERNAME:$USERNAME /home/$USERNAME
-}
-
-sublime_text_config()
-{
-    echo "setting up sublime text 2..."
-#     # Sublime Text 2
-#     wget -O $PWD/sublime.tar.bz2 "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2"
-#     tar xf sublime.tar.bz2
-#     rm $PWD/*.bz2
-#     mv Sublime* /usr/share/sublime_text
-#     ln -s /usr/share/sublime_text/sublime_text /usr/bin/subl
-#     echo "[Desktop Entry]\nName=Sublime Text 2\nComment=The Best Text Editor in the World!\nTryExec=subl\nExec=subl\nIcon=/usr/share/sublime_text/Icon/256x256/sublime_text.png\nType=Application\nCategories=Office;Sublime Text;" > /usr/share/applications/subl.desktop
-#     echo "text/plain=subl.desktop\ntext/css=subl.desktop\ntext/htm=subl.desktop\ntext/javascript=subl.desktop\ntext/x-c=subl.desktop\ntext/csv=subl.desktop\ntext/x-java-source=subl.desktop\ntext/java=subl.desktop\n" >> /usr/share/applications/defaults.list
-#     update-desktop-database
-
-#     # Add User Configuration
-#     if [ -d $FILES/sublime_text ] && [ ! -z "$USERNAME" ];then
-#         mkdir -p /home/$USERNAME/.config/sublime-text-2
-#         cp -R $FILES/sublime_text/* /home/$USERNAME/.config/sublime-text-2/
-#         chown -R $USERNAME:$USERNAME /home/$USERNAME/.config/
-#     fi
-}
-
-create_user()
-{
-    if [ -n "$PASSWORD" ];then
-        useradd -m -s /bin/bash -p $(mkpasswd -m md5 $PASSWORD) $USERNAME
-    else
-        echo "You will have to add a password to the $USERNAME from root with passwd..."
-        useradd -m -s /bin/bash $USERNAME
-    fi
-}
-
-user_configuration()
-{
-    if [ -n "$USERNAME" ]; then
-        echo "Configure Supplied User: $USERNAME."
-
-        # Create user if not exists
-        if ! id -u "$USERNAME" >/dev/null 2>&1;then
-            echo "creating user: $USERNAME..."
-            create_user
-        fi
-
-        if [ -n "$HAS_SUDO_PRIVS" ] && $HAS_SUDO_PRIVS;then
-            echo "Adding user to sudo group..."
-            usermod -aG sudo $USERNAME
-        fi
-    fi
-}
-
-
-
-
-
-
-
-
-
 
 
 
@@ -331,6 +145,177 @@ setup_firewall()
 
 
 
+
+guake_config()
+{
+    echo "patching guake notification..."
+    sed -i 's/notification.show()/try:\n                notification.show()\n            except Exception:\n                pass/' /usr/bin/guake
+    rm /etc/xdg/autostart/guake.desktop
+    sed -i '/StartupNotify|X-GNOME-Autostart-enabled/d' /usr/share/applications/guake.desktop
+
+    echo "setting guake automation at boot time..."
+    mkdir -p /root/.config/autostart
+    ln -s /usr/share/applications/guake.desktop /root/.config/autostart/guake.desktop
+    if [ -n "$USERNAME" ];then
+        mkdir -p /home/$USERNAME/.config/autostart
+        ln -s /usr/share/applications/guake.desktop /home/$USERNAME/.config/autostart/guake.destop
+        chown -R $USERNAME:$USERNAME /home/$USERNAME/.config/autostart
+    fi
+}
+
+sublime_text_config()
+{
+    echo "downloading, installing and configuring sublime text 2..."
+    wget -O $DEV_DIR/sublime.tar.bz2 "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2"
+    tar xf $DEV_DIR/sublime.tar.bz2
+    rm $DEV_DIR/sublime.tar.bz2
+    mv Sublime* /usr/share/sublime_text
+    ln -s /usr/share/sublime_text/sublime_text /usr/bin/subl
+    echo "[Desktop Entry]\nName=Sublime Text 2\nComment=The Best Text Editor in the World!\nTryExec=subl\nExec=subl\nIcon=/usr/share/sublime_text/Icon/256x256/sublime_text.png\nType=Application\nCategories=Office;Sublime Text;" > /usr/share/applications/subl.desktop
+    echo "text/plain=subl.desktop\ntext/css=subl.desktop\ntext/htm=subl.desktop\ntext/javascript=subl.desktop\ntext/x-c=subl.desktop\ntext/csv=subl.desktop\ntext/x-java-source=subl.desktop\ntext/java=subl.desktop\n" >> /usr/share/applications/defaults.list
+    update-desktop-database
+
+    if [ -d "$FILES/sublime_text" ] && [ -n "$USERNAME" ];then
+        echo "loading sublime configuration files..."
+        mkdir -p /home/$USERNAME/.config/sublime-text-2
+        cp -R $FILES/sublime_text/* /home/$USERNAME/.config/sublime-text-2/
+        chown -R $USERNAME:$USERNAME /home/$USERNAME/.config/
+    fi
+}
+
+gui_configuration()
+{
+    echo "running gui configuration..."
+    sublime_text_config
+    guake_config
+}
+
+user_git_configuration()
+{
+    if [ -n "$CONFIGURE_GIT" ] && $CONFIGURE_GIT;then
+        echo "configuring git..."
+        if [ ! -z "$GIT_NAME" ];then
+            git config --global user.name "$GIT_NAME"
+        fi
+        if [ ! -z "$GIT_EMAIL" ];then
+            git config --global user.email "$GIT_EMAIL"
+        fi
+        git config --global core.editor "vim"
+        git config --global help.autocorrect -1
+        git config --global color.ui true
+        su -c "git config --global user.name \"$GIT_NAME\"" $USERNAME
+        su -c "git config --global user.email \"$GIT_EMAIL\"" $USERNAME
+        su -c "git config --global core.editor \"vim\"" $USERNAME
+        su -c "git config --global help.autocorrect -1" $USERNAME
+        su -c "git config --global color.ui true" $USERNAME
+    fi
+}
+
+create_user()
+{
+    if [ -n "$PASSWORD" ];then
+        useradd -m -s /bin/bash -p $(mkpasswd -m md5 $PASSWORD) $USERNAME
+    else
+        echo "you will have to add a password to the $USERNAME from root with passwd..."
+        useradd -m -s /bin/bash $USERNAME
+    fi
+}
+
+user_configuration()
+{
+    if [ -n "$USERNAME" ]; then
+        echo "configuring supplied user: $USERNAME..."
+
+        if ! id -u "$USERNAME" >/dev/null 2>&1;then
+            echo "creating user: $USERNAME..."
+            create_user
+        fi
+
+        if [ -n "$HAS_SUDO_PRIVS" ] && $HAS_SUDO_PRIVS;then
+            echo "adding user to sudo group..."
+            usermod -aG sudo $USERNAME
+        fi
+        user_git_configuration
+    fi
+}
+
+load_git_config()
+{
+    if [ -n "$CONFIGURE_GIT" ] && $CONFIGURE_GIT;then
+        echo "adding and installing git helpers..."
+        cp -ra $FILES/git/. /etc/skel
+        cp -ra $FILES/git/. /root
+        echo "\n\t[alias]\n\t\tl = \"!. ~/.githelpers && pretty_git_log\"" >> /root/.gitconfig
+        echo "\n# Git Additions\n. ~/.git-completion\n. ~/.git-prompt" >> /root/.profile
+        echo "\n\t[alias]\n\t\tl = \"!. ~/.githelpers && pretty_git_log\"" >> /etc/skel/.gitconfig
+        echo "\n# Git Additions\n. ~/.git-completion\n. ~/.git-prompt" >> /etc/skel/.profile
+    fi
+}
+
+install_fonts()
+{
+    if [ -n "$INSTALL_FONTS" ] && $INSTALL_FONTS;then
+        echo "installing custom fonts..."
+        mkdir -p /usr/share/fonts/truetype
+        if [ -d $FILES/fonts/ ];then
+            mv $FILES/fonts/*.ttf /usr/share/fonts/truetype
+            fc-cache -rf
+        fi
+    fi
+}
+
+add_jis_locale()
+{
+    PACKAGES="$PACKAGES fonts-takao"
+    echo "ja_JP.UTF-8 UTF-8" >> /etc/locale.gen
+    locale-gen
+}
+
+ssh_config()
+{
+    if [ -n "$SSH_PORT" ];then
+        echo "updating ssh port..."
+        sed -i "s/Port 22/Port $SSH_PORT/" /etc/ssh/sshd_config
+        service ssh restart
+    fi
+}
+
+ssd_trim_config()
+{
+    if [ -n "TRIM" ] && $TRIM; then
+        echo "adding trim to ssh & weekly crontab for file systems..."
+        sed -i 's/issue_discards = 0/issue_discards = 1/' /etc/lvm/lvm.conf
+        echo "#!/bin/sh\nfor mount in / /boot /home /var/log /tmp; do\n\tfstrim $mount\ndone" > /etc/cron.weekly/fstab
+        chmod +x /etc/cron.weekly/fstab
+    fi
+}
+
+setup_automatic_updates()
+{
+    if [ -n "$AUTOMATIC_UPDATES" ] && $AUTOMATIC_UPDATES;then
+    echo "creating automatic updates and adding to executable crontabs..."
+    echo "#!/bin/sh" >> /etc/cron.weekly/aptitude
+    echo "# Weekly Software Update Processing" >> /etc/cron.weekly/aptitude
+    echo "aptitude clean" >> /etc/cron.weekly/aptitude
+    echo "aptitude update" >> /etc/cron.weekly/aptitude
+    echo "aptitude upgrade -y || aptitude upgrade -y" >> /etc/cron.weekly/aptitude
+    echo "aptitude safe-upgrade -y || aptitude safe-upgrade -y" >> /etc/cron.weekly/aptitude
+    chmod +x /etc/cron.weekly/aptitude
+}
+
+template_configuration()
+{
+    echo "configuring to match template..."
+    setup_automatic_updates
+    ssd_trim_config
+    ssh_config
+    add_jis_locale
+    install_fonts
+    load_git_config
+    user_configuration
+    gui_configuration
+}
+
 install_packages()
 {
     echo "cleaning up aptitude..."
@@ -354,14 +339,12 @@ install_packages()
     aptitude install -y $PACKAGES
     aptitude install -y $PACKAGES
 
-    echo "package installation completed."
+    echo "package installation completed..."
 }
 
 add_template_packages()
 {
-    echo "adding software packages..."
-
-    # Basic System Packages
+    echo "adding basic software packages..."
     PACKAGES="$PACKAGES screen"
     PACKAGES="$PACKAGES tmux"
     PACKAGES="$PACKAGES ssh"
@@ -421,4 +404,3 @@ add_template_packages()
         fi
     fi
 }
-
