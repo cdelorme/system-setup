c14145cc59c5b8c3d2d0d08f0d1c89996990e3c3
Casey DeLorme <cdelorme@gmail.com>
2014-12-08 00:39:24 -0500

    added some outlining for web script

---
+++ scripts/linux/web/dotdeb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# add dotdeb repository
+$dl_cmd http://www.dotdeb.org/dotdeb.gpg
+cat dotdeb.gpg | apt-key add -
+rm dotdeb.gpg
+
+# add to sources
+echo "deb http://packages.dotdeb.org wheezy all" > /etc/apt/sources.list.d/dotdeb.list
+echo "deb-src http://packages.dotdeb.org wheezy all" >> /etc/apt/sources.list.d/dotdeb.list
--- scripts/web.sh
+++ scripts/web.sh
@@ -1,1 +1,45 @@
 #!/bin/bash
+
+# load template
+[ -f "scripts/template.sh" ] && . "scripts/template.sh" || . <($source_cmd "${remote_source}scripts/template.sh")
+
+##
+# web-specific operations
+##
+
+# add new groups, and to user
+groupadd -f www-data
+groupadd -f gitdev
+groupadd -f webdev
+usermod -aG webdev,gitdev $username
+
+# create environment folders & set permissions /w sticky bits
+mkdir -p /srv/{www,git}
+chown -R www-data:www-data /srv
+chown -R www-data:webdev /srv/www
+chown -R www-data:gitdev /srv/git
+chmod -R 6775 /srv
+
+# download logrotate for websites
+[ -f "data/etc/logrotate.d/websites" ] && cp "data/etc/logrotate.d/websites" "/etc/logrotate.d/websites"  || $dl_cmd "/etc/monit/monitrc.d/web" "${remote_source}data/etc/logrotate.d/websites"
+
+# adjust iptables for http/https traffic
+sed -i 's/#-A INPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW -j ACCEPT/-A INPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW -j ACCEPT/' /etc/iptables/iptables.rules
+
+
+##
+# modular scripts
+##
+
+# load dotdeb repository
+[ -f "scripts/linux/web/dotdeb.sh" ] && . "scripts/linux/web/dotdeb.sh" || . <($source_cmd "${remote_source}scripts/linux/web/dotdeb.sh")
+
+# include nginx script
+[ -f "scripts/linux/web/nginx.sh" ] && . "scripts/linux/web/nginx.sh" || . <($source_cmd "${remote_source}scripts/linux/web/nginx.sh")
+
+# @todo include databases (mongodb & mariabd)
+[ -f "scripts/linux/web/mongodb.sh" ] && . "scripts/linux/web/mongodb.sh" || . <($source_cmd "${remote_source}scripts/linux/web/mongodb.sh")
+[ -f "scripts/linux/web/mariadb.sh" ] && . "scripts/linux/web/mariadb.sh" || . <($source_cmd "${remote_source}scripts/linux/web/mariadb.sh")
+
+# msmtp mail server
+[ -f "scripts/linux/web/msmtp.sh" ] && . "scripts/linux/web/msmtp.sh" || . <($source_cmd "${remote_source}scripts/linux/web/msmtp.sh")
