59098452b3c2adf453f067bb53614298c4906dbd
Casey DeLorme <cdelorme@gmail.com>
2014-12-08 02:32:19 -0500

    added msmtp script module

---
+++ scripts/linux/web/msmtp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# verify desire to install
+[ "$install_msmtp" = "y" ] || exit 0
+
+# install msmtp-mta and all related/useful components
+aptitude install -ryq msmtp-mta
+
+# download msmtp-mta sample config & modify
+[ -f "data/etc/msmtprc" ] && cp "data/etc/msmtprc" "/etc/msmtprc"  || $dl_cmd "/etc/msmtprc" "${remote_source}data/etc/msmtprc"
+
+# set restricted permissions (file will contain plain-text password)
+chmod 0600 /etc/msmtprc
+
+# set email & password
+sed -i "s/user username/user $msmtp_username/" /etc/msmtprc
+sed -i "s/password password/password $msmtp_password/" /etc/msmtprc
