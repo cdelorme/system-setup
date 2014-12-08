015bccde95755c4ff76a5c9fd3077e0b43ac7ed7
Casey DeLorme <cdelorme@gmail.com>
2014-12-08 00:46:48 -0500

    added operations to clean and update resources

--- scripts/linux/web/dotdeb.sh
+++ scripts/linux/web/dotdeb.sh
@@ -8,3 +8,7 @@ rm dotdeb.gpg
 # add to sources
 echo "deb http://packages.dotdeb.org wheezy all" > /etc/apt/sources.list.d/dotdeb.list
 echo "deb-src http://packages.dotdeb.org wheezy all" >> /etc/apt/sources.list.d/dotdeb.list
+
+# update resources
+aptitude clean
+aptitude update
