ce22eaad54554bb01d657ca8764a1b7a15dd2c87
Casey DeLorme <cdelorme@gmail.com>
2014-12-13 01:42:32 -0500

    finished adjusting package dependencies for compton

--- scripts/linux/gui/compton.sh
+++ scripts/linux/gui/compton.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # install dependencies without "recommended" additions
-aptitude install -yqR asciidoc
+aptitude install -Ryq asciidoc
+
+# dependencies:
+aptitude install -ryq build-essential libxdamage-dev libxcomposite-dev libx11-dev libxfixes-dev libxext-dev libxrender-dev libxrandr-dev libxinerama-dev pkg-config x11-utils libpcre3-dev libconfig-dev libdrm-dev libdbus-1-dev libgl1-mesa-dev x11proto-core-dev
 
 # download source
 git clone https://github.com/chjj/compton.git /tmp/compton
