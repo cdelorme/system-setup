f058115ef2b406d09586676b3bf3aaf3d502067b
Casey DeLorme <cdelorme@gmail.com>
2014-12-13 18:08:08 -0500

    finished setting up samba

--- scripts/linux/comm/samba.sh
+++ scripts/linux/comm/samba.sh
@@ -3,9 +3,12 @@
 # install packages
 aptitude install -ryq samba samba-tools smbclient
 
-# @todo download samba config
+# download samba config
+[ -f "data/etc/samba/smb.conf" ] && cp "data/etc/samba/smb.conf" "/etc/samba/smb.conf"  || $dl_cmd "/etc/samba/smb.conf" "${remote_source}data/etc/samba/smb.conf"
 
 # add user
 usermod -aG sambashare $username
 
-# @todo download samba monit
+# download & install samba monit
+[ -f "data/etc/monit/monitrc.d/samba" ] && cp "data/etc/monit/monitrc.d/samba" "/etc/monit/monitrc.d/samba"  || $dl_cmd "/etc/monit/monitrc.d/samba" "${remote_source}data/etc/monit/monitrc.d/samba"
+ln -nsf "../monitrc.d/samba" "/etc/monit/conf.d/samba"
