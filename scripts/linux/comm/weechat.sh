393a62670932bfe246e82f0f49e0afe032121477
Casey DeLorme <cdelorme@gmail.com>
2014-12-14 19:42:49 -0500

    weechat config, no more username/password entry.

--- scripts/linux/comm/weechat.sh
+++ scripts/linux/comm/weechat.sh
@@ -3,18 +3,14 @@
 # install package
 aptitude install -ryq weechat-ncurses
 
-# @todo download weechat configuration
-# @todo insert username and password
+# prepare weechat directory
+mkdir -p "/home/${username}/.weechat"
 
+# generate weechat config with first-run
+su $username -s /bin/bash -c 'weechat-curses &> /dev/null & pid=$!
+    while ! ([ -f ~/.weechat/irc.conf ] && [ -f ~/.weechat/weechat.conf ]); do :; done;
+    kill -9 $pid'
 
-# # generate dependent weechat files files
-# su $config_system_username -s /bin/bash -c 'weechat-curses &> /dev/null & pid=$!
-#     while ! ([ -f ~/.weechat/irc.conf ] && [ -f ~/.weechat/weechat.conf ]); do :; done;
-#     kill -9 $pid'
-
-# # set configuration values
-# mkdir -p "${user_home_dir}/.weechat"
-# sed -i 's/max_buffer_lines_number.*/max_buffer_lines_number = 0/' "${user_home_dir}/.weechat/weechat.conf"
-# sed -i 's/freenode\.autoconnect.*/freenode\.autoconnect = on/' "${user_home_dir}/.weechat/irc.conf"
-# sed -i "s/freenode\.nicks.*/freenode\.nicks = \"${config_irc_username}, ${config_irc_username}_\"/" "${user_home_dir}/.weechat/irc.conf"
-# sed -i "s/freenode\.password.*/freenode\.password = \"${config_irc_password}\"/" "${user_home_dir}/.weechat/irc.conf"
+# configure weechat
+sed -i 's/max_buffer_lines_number.*/max_buffer_lines_number = 0/' "/home/${username}/.weechat/weechat.conf"
+sed -i 's/freenode\.autoconnect.*/freenode\.autoconnect = on/' "/home/${username}/.weechat/irc.conf"
