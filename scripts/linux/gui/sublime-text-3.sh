c83b9c5ad855f264f203b39fb189b79d087f6ecc
Casey DeLorme <cdelorme@gmail.com>
2014-12-14 19:43:08 -0500

    finished sublime text installation steps

--- scripts/linux/gui/sublime-text-3.sh
+++ scripts/linux/gui/sublime-text-3.sh
@@ -1,53 +1,29 @@
 #!/bin/bash
 
-# # download and install sublime text for system user
-# curl -o /tmp/sublime.tar.bz2 http://c758482.r82.cf2.rackcdn.com/sublime_text_3_build_3059_x64.tar.bz2
-# tar xf /tmp/sublime.tar.bz2 -C /tmp
-# rm /tmp/sublime.tar.bz2
+# download & extract sublime text
+$dl_cmd /tmp/sublime.tar.bz2 http://c758482.r82.cf2.rackcdn.com/sublime_text_3_build_3065_x64.tar.bz2
+tar xf /tmp/sublime.tar.bz2 -C /tmp
+rm /tmp/sublime.tar.bz2
 
-# # modify all of the below commands, remove the su, and append the path
-# chown -R $config_system_username:$config_system_username /tmp/sublime_text_3
-# mkdir -p "${user_home_dir}/applications" "${user_home_dir}/.bin" "${user_home_dir}/.config/sublime-text-3/Packages/User" "${user_home_dir}/.config/sublime-text-3/Installed Packages/"
-# mv /tmp/sublime_text_3 "${user_home_dir}/applications/sublime_text"
-# ln -s "${user_home_dir}/applications/sublime_text/sublime_text" "${user_home_dir}/.bin/subl"
+# install global copy
+cp -R /tmp/sublime_text_3 /usr/local/sublime-text
+ln -nsf /usr/local/sublime-text/sublime_text /usr/sbin/subl
 
-# # install sublime package control
-# curl -o "${user_home_dir}/.config/sublime-text-3/Installed Packages/Package Control.sublime-package" "https://sublime.wbond.net/Package%20Control.sublime-package"
+# install user copy
+mkdir -p /home/$username/applications /home/$username/.bin
+cp -R /tmp/sublime_text_3 /home/$username/applications/sublime-text
+ln -nsf /home/$username/applications/sublime-text/sublime_text /home/$username/.bin/subl
 
-# # populate sublime preferences
-# echo '{' > "${user_home_dir}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
-# echo '    "auto_complete_commit_on_tab": true,' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
-# echo '    "caret_style": "phase",' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
-# echo '    "color_scheme": "Packages/Color Scheme - Default/Sunburst.tmTheme",' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
-# echo '    "font_face": "ForMateKonaVe",' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
-# echo '    "font_size": 14,' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
-# echo '    "highlight_line": true,' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
-# echo '    "highlight_modified_tabs": true,' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
-# echo '    "match_brackets_angle": true,' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
-# echo '    "scroll_past_end": true,' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
-# echo '    "scroll_speed": 2.0,' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
-# echo '    "translate_tabs_to_spaces": true,' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
-# echo '    "trim_trailing_white_space_on_save": true' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
-# echo '}' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
+# install sublime package control
+$dl_cmd "/home/$username/.config/sublime-text-3/Installed Packages/Package Control.sublime-package" "https://sublime.wbond.net/Package%20Control.sublime-package"
 
-# # configure hotkeys (some are plugin dependent and will simply not work)
-# echo '[' > "${user_home_dir}/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap"
-# echo '    { "keys": ["ctrl+tab"], "command": "next_view" },' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap"
-# echo '    { "keys": ["ctrl+shift+tab"], "command": "prev_view" },' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap"
-# echo ']' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap"
-# # echo '[' > "${user_home_dir}/.config/sublime-text-3/Packages/User/Default (OSX).sublime-keymap"
-# # echo '    { "keys": ["ctrl+tab"], "command": "next_view" },' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Default (OSX).sublime-keymap"
-# # echo '    { "keys": ["ctrl+shift+tab"], "command": "prev_view" },' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Default (OSX).sublime-keymap"
-# # echo ']' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/Default (OSX).sublime-keymap"
+# download configuration files
+[ -f "data/home/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap" ] && cp "data/home/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap" "/home/${username}/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap"  || $dl_cmd "/home/${username}/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap" "${remote_source}data/home/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap"
+[ -f "data/home/.config/sublime-text-3/Packages/User/Default (OSX).sublime-keymap" ] && cp "data/home/.config/sublime-text-3/Packages/User/Default (OSX).sublime-keymap" "/home/${username}/.config/sublime-text-3/Packages/User/Default (OSX).sublime-keymap"  || $dl_cmd "/home/${username}/.config/sublime-text-3/Packages/User/Default (OSX).sublime-keymap" "${remote_source}data/home/.config/sublime-text-3/Packages/User/Default (OSX).sublime-keymap"
+[ -f "data/home/.config/sublime-text-3/Packages/User/Default (Windows).sublime-keymap" ] && cp "data/home/.config/sublime-text-3/Packages/User/Default (Windows).sublime-keymap" "/home/${username}/.config/sublime-text-3/Packages/User/Default (Windows).sublime-keymap"  || $dl_cmd "/home/${username}/.config/sublime-text-3/Packages/User/Default (Windows).sublime-keymap" "${remote_source}data/home/.config/sublime-text-3/Packages/User/Default (Windows).sublime-keymap"
+[ -f "data/home/.config/sublime-text-3/Packages/User/Preferences.sublime-settings" ] && cp "data/home/.config/sublime-text-3/Packages/User/Preferences.sublime-settings" "/home/${username}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"  || $dl_cmd "/home/${username}/.config/sublime-text-3/Packages/User/Preferences.sublime-settings" "${remote_source}data/home/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
+[ -f "data/home/.config/sublime-text-3/Packages/User/default_file_type.sublime-settings" ] && cp "data/home/.config/sublime-text-3/Packages/User/default_file_type.sublime-settings" "/home/${username}/.config/sublime-text-3/Packages/User/default_file_type.sublime-settings"  || $dl_cmd "/home/${username}/.config/sublime-text-3/Packages/User/default_file_type.sublime-settings" "${remote_source}data/home/.config/sublime-text-3/Packages/User/default_file_type.sublime-settings"
+[ -f "data/home/.config/sublime-text-3/Packages/User/GoSublime.sublime-settings" ] && cp "data/home/.config/sublime-text-3/Packages/User/GoSublime.sublime-settings" "/home/${username}/.config/sublime-text-3/Packages/User/GoSublime.sublime-settings"  || $dl_cmd "/home/${username}/.config/sublime-text-3/Packages/User/GoSublime.sublime-settings" "${remote_source}data/home/.config/sublime-text-3/Packages/User/GoSublime.sublime-settings"
+[ -f "data/home/.config/sublime-text-3/Packages/User/MarkdownPreview.sublime-settings" ] && cp "data/home/.config/sublime-text-3/Packages/User/MarkdownPreview.sublime-settings" "/home/${username}/.config/sublime-text-3/Packages/User/MarkdownPreview.sublime-settings"  || $dl_cmd "/home/${username}/.config/sublime-text-3/Packages/User/MarkdownPreview.sublime-settings" "${remote_source}data/home/.config/sublime-text-3/Packages/User/MarkdownPreview.sublime-settings"
 
-# # populate markdown-preview config file
-# echo '{' > "${user_home_dir}/.config/sublime-text-3/Packages/User/MarkdownPreview.sublime-settings"
-# echo '    "build_action": "browser"' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/MarkdownPreview.sublime-settings"
-# echo '}' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/MarkdownPreview.sublime-settings"
-
-# # populate go-sublime package config
-# echo '{' > "${user_home_dir}/.config/sublime-text-3/Packages/User/GoSublime.sublime-settings"
-# echo '    "env": {' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/GoSublime.sublime-settings"
-# echo '        "GOPATH": "$HOME/.go"' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/GoSublime.sublime-settings"
-# echo '    }' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/GoSublime.sublime-settings"
-# echo '}' >> "${user_home_dir}/.config/sublime-text-3/Packages/User/GoSublime.sublime-settings"
+# installation of plugins is left to the user
