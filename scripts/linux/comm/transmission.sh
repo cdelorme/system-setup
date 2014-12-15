a75692e971b0575ebb4c7d5af720c963ed8993f1
Casey DeLorme <cdelorme@gmail.com>
2014-12-14 19:20:46 -0500

    finished transmission setup

--- scripts/linux/comm/transmission.sh
+++ scripts/linux/comm/transmission.sh
@@ -3,13 +3,17 @@
 # install transmission
 aptitude install -ryq transmission transmission-cli transmission-daemon
 
-# @todo configure transmission
-# config_bt_max_down=3000
-# config_bt_max_up=80
-# config_bt_watch_path="/tmp"
-# config_bt_incomplete_path="/tmp"
-# config_bt_complete_path="/tmp"
-# config_bt_web_accessible=true
-# config_bt_web_port=9010
+# stop transmission daemon (to prevent settings from being overwritten)
+service transmission-daemon stop
 
-# @todo download transmission monit
+# download base transmission config
+[ -f "data/etc/transmission-daemon/settings.json" ] && cp "data/etc/transmission-daemon/settings.json" "/etc/transmission-daemon/settings.json"  || $dl_cmd "/etc/transmission-daemon/settings.json" "${remote_source}data/etc/transmission-daemon/settings.json"
+
+# attempt to restart transmission daemon
+service transmission-daemon start
+service transmission-daemon restart
+
+# download transmission monit
+[ -f "data/etc/monit/monitrc.d/transmission-daemon" ] && cp "data/etc/monit/monitrc.d/transmission-daemon" "/etc/monit/monitrc.d/transmission-daemon"  || $dl_cmd "/etc/monit/monitrc.d/transmission-daemon" "${remote_source}data/etc/monit/monitrc.d/transmission-daemon"
+ln -nsf "../monitrc.d/transmission-daemon" "/etc/monit/conf.d/transmission-daemon"
+monit -t && service monit restart
