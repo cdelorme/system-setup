8cce21d4aac9621c4ebcbcb8801aac1c47150de7
Casey DeLorme <cdelorme@gmail.com>
2014-12-16 10:44:16 -0500

    moved package from openbox to wireless

--- scripts/linux/workstation/wireless.sh
+++ scripts/linux/workstation/wireless.sh
@@ -2,3 +2,9 @@
 
 # install wireless utility packages
 aptitude install -ryq avahi-utils avahi-daemon libnss-mdns wireless-tools bluez bluez-utils bluez-tools
+
+# conditionally install wireless audio package
+if [ "$install_openbox" = "y" ]
+then
+    aptitude install -ryq pulseaudio-module-bluetooth
+fi
