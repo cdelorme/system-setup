761e29b61e45fa2dde76992edae1c3d965c5c99d
Casey DeLorme <cdelorme@gmail.com>
2014-12-16 18:23:05 -0500

    removed step that "cleans up" the files post-install
    will happen automatically on reboot, but we want to have access just in
    case

--- scripts/linux/workstation/youtube-dl.sh
+++ scripts/linux/workstation/youtube-dl.sh
@@ -6,11 +6,8 @@ git clone https://github.com/rg3/youtube-dl /tmp/youtube-dl
 # build & install
 (cd /tmp/youtube-dl && python setup.py install)
 
-# cleanup source files
-rm -rf /tmp/youtube-dl
-
 # first-run updates
 youtube-dl -U
 
 # append to system updates
-[ -f "/etc/cron.daily/system-updates" ] && echo "youtube-dl -U" >> "/etc/cron.daily/system-updates"
+[ -f "/etc/cron.daily/system-updates" ] && echo "which youtube-dl &>/dev/null && youtube-dl -U" >> "/etc/cron.daily/system-updates"
