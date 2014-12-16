f1bf0e114ab8146217b6acb8cd2e4d8320babd01
Casey DeLorme <cdelorme@gmail.com>
2014-12-16 18:21:51 -0500

    added jessie-specific dependencies

--- scripts/linux/gui/compton.sh
+++ scripts/linux/gui/compton.sh
@@ -4,13 +4,10 @@
 aptitude install -Ryq asciidoc
 
 # dependencies:
-aptitude install -ryq build-essential libxdamage-dev libxcomposite-dev libx11-dev libxfixes-dev libxext-dev libxrender-dev libxrandr-dev libxinerama-dev pkg-config x11-utils libpcre3-dev libconfig-dev libdrm-dev libdbus-1-dev libgl1-mesa-dev x11proto-core-dev
+aptitude install -ryq build-essential libxdamage-dev libxcomposite-dev libx11-dev libxfixes-dev libxext-dev libxrender-dev libxrandr-dev libxinerama-dev pkg-config x11-utils libpcre3-dev libconfig-dev libdrm-dev libdbus-1-dev libgl1-mesa-dev x11proto-core-dev libxml2-utils xsltproc
 
 # download source
 git clone https://github.com/chjj/compton.git /tmp/compton
 
 # build & install
 (cd /tmp/compton && make && make install)
-
-# cleanup
-rm -rf /tmp/compton
