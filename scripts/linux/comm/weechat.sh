be549dd8342c094ed4e3b8c85a08549919afee5f
Casey DeLorme <cdelorme@gmail.com>
2014-12-16 11:56:37 -0500

    should have fixed the execution & halting of weechat to generate initial config files

--- scripts/linux/comm/weechat.sh
+++ scripts/linux/comm/weechat.sh
@@ -7,9 +7,7 @@ aptitude install -ryq weechat
 mkdir -p "/home/${username}/.weechat"
 
 # generate weechat config with first-run
-su $username -s /bin/bash -c 'weechat-curses &> /dev/null & pid=$!
-    while ! ([ -f ~/.weechat/irc.conf ] && [ -f ~/.weechat/weechat.conf ]); do :; done;
-    kill -9 $pid'
+su $username -s /bin/bash -c 'weechat-curses &>/dev/null & pid=$!; sleep 10; kill -9 $pid'
 
 # configure weechat
 sed -i 's/max_buffer_lines_number.*/max_buffer_lines_number = 0/' "/home/${username}/.weechat/weechat.conf"
