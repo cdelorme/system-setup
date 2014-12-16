a6f3ed78ab7a9d6735fdd111921843c991033930
Casey DeLorme <cdelorme@gmail.com>
2014-12-16 18:22:54 -0500

    added jessie-specific packages

--- scripts/linux/workstation/wireless.sh
+++ scripts/linux/workstation/wireless.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install wireless utility packages
-aptitude install -ryq avahi-utils avahi-daemon libnss-mdns wireless-tools bluez bluez-utils bluez-tools
+aptitude install -ryq avahi-utils avahi-daemon libnss-mdns wireless-tools bluez bluez-utils bluez-tools bluez-firmware bluez-hcidump
 
 # conditionally install wireless audio package
 if [ "$install_openbox" = "y" ]
