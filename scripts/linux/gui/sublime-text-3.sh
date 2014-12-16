c365216b6f41ce87ed92b2c487a8bcb1d3627c4e
Casey DeLorme <cdelorme@gmail.com>
2014-12-16 10:43:56 -0500

    adjusted sublime text 3 install, forgot to make containing folders

--- scripts/linux/gui/sublime-text-3.sh
+++ scripts/linux/gui/sublime-text-3.sh
@@ -10,14 +10,16 @@ cp -R /tmp/sublime_text_3 /usr/local/sublime-text
 ln -nsf /usr/local/sublime-text/sublime_text /usr/sbin/subl
 
 # install user copy
-mkdir -p /home/$username/applications /home/$username/.bin
-cp -R /tmp/sublime_text_3 /home/$username/applications/sublime-text
-ln -nsf /home/$username/applications/sublime-text/sublime_text /home/$username/.bin/subl
+mkdir -p "/home/${username}/applications" "/home/${username}/.bin"
+cp -R "/tmp/sublime_text_3" "/home/${username}/applications/sublime-text"
+ln -nsf "/home/${username}/applications/sublime-text/sublime_text" "/home/${username}/.bin/subl"
 
 # install sublime package control
-$dl_cmd "/home/$username/.config/sublime-text-3/Installed Packages/Package Control.sublime-package" "https://sublime.wbond.net/Package%20Control.sublime-package"
+mkdir -p "/home/${username}/.config/sublime-text-3/Installed Packages/"
+$dl_cmd "/home/${username}/.config/sublime-text-3/Installed Packages/Package Control.sublime-package" "https://sublime.wbond.net/Package%20Control.sublime-package"
 
 # download configuration files
+mkdir -p "/home/${username}/.config/sublime-text-3/Packages/User"
 [ -f "data/home/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap" ] && cp "data/home/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap" "/home/${username}/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap"  || $dl_cmd "/home/${username}/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap" "${remote_source}data/home/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap"
 [ -f "data/home/.config/sublime-text-3/Packages/User/Default (OSX).sublime-keymap" ] && cp "data/home/.config/sublime-text-3/Packages/User/Default (OSX).sublime-keymap" "/home/${username}/.config/sublime-text-3/Packages/User/Default (OSX).sublime-keymap"  || $dl_cmd "/home/${username}/.config/sublime-text-3/Packages/User/Default (OSX).sublime-keymap" "${remote_source}data/home/.config/sublime-text-3/Packages/User/Default (OSX).sublime-keymap"
 [ -f "data/home/.config/sublime-text-3/Packages/User/Default (Windows).sublime-keymap" ] && cp "data/home/.config/sublime-text-3/Packages/User/Default (Windows).sublime-keymap" "/home/${username}/.config/sublime-text-3/Packages/User/Default (Windows).sublime-keymap"  || $dl_cmd "/home/${username}/.config/sublime-text-3/Packages/User/Default (Windows).sublime-keymap" "${remote_source}data/home/.config/sublime-text-3/Packages/User/Default (Windows).sublime-keymap"
