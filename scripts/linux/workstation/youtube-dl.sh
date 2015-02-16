97afc8a54acd7dffdc1f1fea02264a56b9a82a2f
Casey DeLorme <cdelorme@USD1400997.local>
2015-02-16 09:56:15 -0500

    add steps to install youtube-dl from package manager for jessie

--- scripts/linux/workstation/youtube-dl.sh
+++ scripts/linux/workstation/youtube-dl.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# download source
-git clone https://github.com/rg3/youtube-dl /tmp/youtube-dl
+# try to install it via package manager first
+aptitude install -ryq youtube-dl
 
-# build & install
-(cd /tmp/youtube-dl && python setup.py install)
+if ! which youtube-dl &>/dev/null
+then
 
-# first-run updates
-youtube-dl -U
+    # download source
+    git clone https://github.com/rg3/youtube-dl /tmp/youtube-dl
 
-# append to system updates
-[ -f "/etc/cron.daily/system-updates" ] && echo "which youtube-dl &>/dev/null && youtube-dl -U" >> "/etc/cron.daily/system-updates"
+    # build & install
+    (cd /tmp/youtube-dl && python setup.py install)
+
+    # first-run updates
+    youtube-dl -U
+
+    # append to system updates
+    [ -f "/etc/cron.daily/system-updates" ] && echo "which youtube-dl &>/dev/null && youtube-dl -U" >> "/etc/cron.daily/system-updates"
+fi
